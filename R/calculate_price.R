31fb50205506a528e38260280bab15d4f8512067
jeremy-haynes <126425405+jeremy-haynes@users.noreply.github.com>
2024-10-09 18:00:12 -0400

    updated price calculator

--- R/calculate_price.R
+++ R/calculate_price.R
@@ -1,7 +1,6 @@
-calculate_price = function(alpha, prices = seq(.01, 100, .01), Q0_S = 100){
+calculate_price = function(alpha, prices = seq(.01, 100, .01), Q0_S = 100, divisor = 2){
   data.frame(price = prices, demand = calculate_QS(prices, alpha_S = alpha)) %>%
-    mutate(log_price = log10(price), log_demand = log10(demand),
-           euclidean_distance = sqrt((log_demand - (Q0_S/2))^2),
+    mutate(euclidean_distance = sqrt((demand - (Q0_S/divisor))^2),
            new_price = min(euclidean_distance, na.rm = T) == euclidean_distance) %>%
     dplyr::filter(new_price) %>% .$price %>% return()
 }
